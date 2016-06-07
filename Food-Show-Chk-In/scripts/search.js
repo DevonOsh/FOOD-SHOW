fadc271e987a4d74005a38ec7fd7b7a52c2cff82
Devon O'Shaughnessy <devono@ulfoods.com>
2016-06-07 20:46:48 +0000

    In search.js, registeredAction now happens on Enter click instead of Select.

--- Food-Show-Chk-In/scripts/search.js
+++ Food-Show-Chk-In/scripts/search.js
@@ -2,6 +2,7 @@
 ///<reference path="~/kendo/js/kendo.all.min.js"/>
 (function (fsr, $) {
     var search = null,
+        isRegistered,
         app = fsr.app = fsr.app || {};
 
     app.search = {
@@ -10,17 +11,21 @@
 
             $("#name-autocomplete").kendoAutoComplete({
                 dataSource: customerData,
-                select: app.search.onSelect,
+                select: function(e){
+                    var dataItem = this.dataItem(e.item.index());
+                    isRegistered = dataItem.Registered;
+                },
                 placeholder: "Enter company name",
                 dataTextField: "Name",
                 minLength: 1
             });
+            
+            $("#name-button").kendoButton({
+                click: app.search.onClick
+            });
            
         },
-        onSelect: function (e) {
-            var isRegistered;
-            var dataItem = this.dataItem(e.item.index());
-            isRegistered = dataItem.Registered;
+        onClick: function (e) {
             app.registeredAction(isRegistered);
         },
         onHide: function () {
